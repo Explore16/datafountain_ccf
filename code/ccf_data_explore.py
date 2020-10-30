f34de8a855dc957c684e6251f32e8454dc2792eb
Shuaiyicao <shuaiyicao@foxmail.com>
2020-10-30 17:07:55 +0800

    update python file

---
+++ code/ccf_data_explore.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   ccf_data_explore.py    
+@Contact :   caoshuaiyi@brdc.icbc.com.cn, shuaiyicao@foxmail.com
+@License :   None
+
+@Modify Time      @Author    @Version    @Desciption
+------------      -------    --------    -----------
+2020/10/30 17:07   shuaiyicao    1.0         None
+'''
