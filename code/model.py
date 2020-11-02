dfe3ebbd9dfd0e8745fe022b49c8022a6122f659
Shuaiyicao <shuaiyicao@foxmail.com>
2020-11-02 16:12:25 +0800

    update fea explore and create model file

---
+++ code/model.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   model.py    
+@Contact :   caoshuaiyi@brdc.icbc.com.cn, shuaiyicao@foxmail.com
+@License :   None
+
+@Modify Time      @Author    @Version    @Desciption
+------------      -------    --------    -----------
+2020/11/2 16:06   shuaiyicao    1.0         None
+'''
